56134dbca6f2fb9bf973c968ff4a302d99909134
jasonit8 <52004170+jasonit8@users.noreply.github.com>
2020-03-03 21:54:12 +0700

    đổi change_menu thành change_items

--- python/header.py
+++ python/header.py
@@ -10,16 +10,16 @@ class header():
         f = open('../index_newheader.html',encoding='utf8',mode='w')
         f.write(change)
         f.close()
-    def change_menu(self,menu):
-        for item in range(8): # menu có 8 mục không đổi
-            self.soup.find_all(attrs={'class':'header-link'})[item].string.replace_with(menu[item])
+    def change_items(self,items):
+        for item in range(8): # navbar có 8 mục không đổi
+            self.soup.find_all(attrs={'class':'header-link'})[item].string.replace_with(items[item])
         self.to_html(str(self.soup.prettify()))
     def change_logo(self,url='img/logo.png'):
         self.soup.find(id='logo-img')['src'] = url
         self.to_html(str(self.soup.prettify()))
 
 # ví dụ
-new_menu = ['new home','about','reservations','menu','news','location','elements','buy porto!']
+new_items = ['new home','about','reservations','menu','news','location','elements','buy porto!']
 b = header()
-b.change_menu(new_menu)
+b.change_items(new_items)
 b.change_logo('img/logo2.png')
