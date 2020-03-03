40bd06e5b9761fc30740faf1a7246122a1efad38
jasonit8 <52004170+jasonit8@users.noreply.github.com>
2020-03-03 16:21:29 +0700

    Update header.py

--- python/header.py
+++ python/header.py
@@ -3,12 +3,11 @@ if __name__ == "__main__":
 
 class header():
     def __init__(self):
-        f = open('../index.html',mode='r',encoding='utf8')
-        f = open('github/coffee_forked/index.html',mode='r',encoding='utf8')
+        f = open('../index.html',encoding='utf8',mode='r')
         self.soup = BeautifulSoup(f.read(),features='lxml')
         f.close()
     def to_html(self,change):
-        f = open('../index_newheader.html',mode='w',encoding='utf8')
+        f = open('../index_newheader.html',encoding='utf8',mode='w')
         f.write(change)
         f.close()
     def change_text(self,menu):
